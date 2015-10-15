3e314cf15da094404d7392627db4537d149411b4
yanhuachn <yanhuachn@163.com>
2015-10-15 09:35:43 -0400

    test1
    test1

--- UI-FirstDemo/app/src/main/java/com/example/apple/loginui/GameActivity.java
+++ UI-FirstDemo/app/src/main/java/com/example/apple/loginui/GameActivity.java
@@ -17,6 +17,7 @@ public class GameActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_game, menu);
+        //sss
         return true;
     }
 
